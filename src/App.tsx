583e084728c8983772a0933346a2d50b2af020fb
Segward <gustask@stud.ntnu.no>
2025-03-21 17:19:42 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <p className="pt-8 text-center text-red-500 text-4xl">
+        Hello, Tailwind CSS!
+      </p>
+    </div>
+  )
+}
+
+export default App
